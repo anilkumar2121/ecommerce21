9ff26c05c7a61a6935f1c4bfda432cff0f6457ba
anilkumar2121 <anilcruel2121@gmail.com>
2024-01-23 16:34:04 +0530

    initialcommitof ecommerce

---
+++ src/components/Assets/AllProduct.js
@@ -0,0 +1,47 @@
+import img from "./s1.jpg"
+let all_products =[
+
+    {
+        id: 1,
+        name: "product",
+        category: "washing",
+        image: img,
+        new_price : 50,
+        old_price:80
+    },
+
+    
+    {
+        id: 2,
+        name: "product",
+        category: "washing",
+        image: img,
+        new_price : 50,
+        old_price:80
+    },
+    {
+        id: 3,
+        name: "product",
+        category: "washing",
+        image: img,
+        new_price : 50,
+        old_price:80
+    },
+    {
+        id: 4,
+        name: "product",
+        category: "washing",
+        image: img,
+        new_price : 50,
+        old_price:80
+    },
+    {
+        id: 5,
+        name: "product",
+        category: "washing",
+        image: img,
+        new_price : 50,
+        old_price:80
+    },
+
+]
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Hero.css";
+import handIcon from "../Assets/handicon.jpg";
+import heroine from "../Assets/heroine.jpg";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="hero">
+        <div className="hero-left">
+          <h2>New arrivals Only</h2>
+          <div>
+            <div className="hero-hand-icon">
+              <p>new</p>
+              <img src={handIcon} alt="" height="25px" width="25px" />
+            </div>
+            <p>Collections</p>
+            <p>for everyone</p>
+          </div>
+          <div className="hero-latest-button">
+            <p>Latest Collections</p>
+            {/* <img src={}  alt="" /> */}
+          </div>
+        </div>
+        <div className="hero-right">
+          <img style={{ height: "100vh" }} src={heroine} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
